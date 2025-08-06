c2dc370f0e2ebede69389d4e399d17a197956e68
Steve Cirelli <scirelli+git@gmail.com>
2025-08-05 21:49:24 -0400

    quick save

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/**
+ * Given a list of integers, use a vector and return the median (when sorted, the value in the
+ * middle position) and mode (the value that occurs most often; a hash map will be helpful here) of
+ * the list.
+ */
+
 fn main() {
-    println!("Hello, world!");
+    let mut arr:[u8; 10] = [0; 10];
+    rand::fill(&mut arr[..]);
+    let v = vec!(arr);
+    println!("{v:?}");
 }
