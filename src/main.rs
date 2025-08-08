c4cda3f63771af98d9bd6d3dc321c88a6741da63
Steve Cirelli <scirelli+git@gmail.com>
2025-08-07 23:04:28 -0400

    quick save

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
+mod math;
+
 /**
  * Given a list of integers, use a vector and return the median (when sorted, the value in the
  * middle position) and mode (the value that occurs most often; a hash map will be helpful here) of
  * the list.
  */
-mod math;
-
-fn main() {
+fn test_median() {
     let mut arr:[u8; 10] = [0; 10];
     rand::fill(&mut arr[..]);
     let mut v = Vec::from(arr);
@@ -17,4 +17,23 @@ fn main() {
     let k = math::median(&mut v);
     println!("Median: {k:?}");
     println!("Vec: {v:?}");
+}
+
+/**
+ * Convert strings to pig latin. The first consonant of each word is moved to the end of the word
+ * and ay is added, so first becomes irst-fay. Words that start with a vowel have hay added to the
+ * end instead (apple becomes apple-hay). Keep in mind the details about UTF-8 encoding!
+ */
+fn to_pig_latin(s: &str) -> String {
+    let mut rtn = String::new();
+    for p in String::from(s).split(" ") {
+        rtn.push_str(p);
+        rtn.push_str(" ");
+    }
+    rtn
+}
+
+fn main(){
+    test_median();
+    println!("{}", to_pig_latin(&"steve speaks latin".to_string()));
 }
